577e4f877278f68cbea3cdd84a1cb9bae259356a
Danial-Waheed <daniyalwahied@gmail.com>
2021-08-21 21:47:00 +0500

    9. swtiches and routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 // import styles
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
