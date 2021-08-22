b9952c8ed672c875e3aef3ce161800b9137c1845
Danial-Waheed <daniyalwahied@gmail.com>
2021-08-22 13:22:19 +0500

    11. movie details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import MovieDetail from "./pages/MovieDetails";
 // import styles
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
@@ -18,8 +19,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -13,21 +13,21 @@ const OurWork = () => {
       <Movie>
         <h2>The Athlete</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-athlete">
           <img src={athlete} alt="athlete" />
         </Link>
       </Movie>
       <Movie>
         <h2>The Racer</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/the-racer">
           <img src={theracer} alt="racer" />
         </Link>
       </Movie>
       <Movie>
         <h2>The GoodTimes</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="/work/good-times">
           <img src={goodtimes} alt="times" />
         </Link>
       </Movie>
